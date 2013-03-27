434d1174f12c7c83b7270661ce9103f359762d3a
aj0strow <alexander.ostrow@gmail.com>
2013-03-27 02:44:18 -0400

    fixed utilites, renamed sound-system, properly throttled update mechanism

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,14 +3,10 @@
   var Game = (function() {
     
     var WINDOW_SIZE = 500;
-    var INITIAL_ASTEROID_COUNT = 5;   
-    var totalAsteroids = 5;  
+    var INITIAL_ASTEROID_COUNT = 5;
+    var FPS = 25;
 
     var game = {
-      // Properties
-
-      // frames per second
-      fps: 25,
 
       level: 0,
 
@@ -22,60 +18,32 @@
       asteroidRadius: 20,
       
       init: function(canvasContainer) {
-        var Ship = window.SpaceRocks.Ship;
-        this.paper = new Raphael(canvasContainer, this.windowSize, this.windowSize);
-        this.ship = new Ship(this.paper);
-        console.log(this);
         var SoundSystem = window.SpaceRocks.SoundSystem;
-        this.sounds = new SoundSystem(true);
-        var Asteroids = window.SpaceRocks.Asteroids;
-        for(var i = 0; i < totalAsteroids; i++){
-          this.asteroids[i] = new Asteroids(this.paper);
+        
+        this.paper = new Raphael(canvasContainer, this.windowSize, this.windowSize);
+        this.sounds = new SoundSystem({ voiceMode: true });
+      },
+      
+      start: function() {        
+        var Ship = window.SpaceRocks.Ship;
+        var Asteroid = window.SpaceRocks.Asteroid;
+        
+        this.ship = new Ship(this.paper);
+        
+        for (var i=0; i < INITIAL_ASTEROID_COUNT; i++) {
+          this.asteroids[i] = new Asteroid(this.paper);
         }
-      },
-      
-      start: function() {
-        console.log('Start called!');
+        
         this.resume();
-      },
-      
-      // This is the game loop
-      update: function() {
-          this.ship.update();
-          for(var i = 0; i < this.asteroids.length; i++){
-            this.asteroids[i].updatePosition();
-          }
-          for( var b = 0; b < this.bullets.length; b++ ){
-            if(this.bullets[b].updatePosition()){
-              this.bullets[b].obj.remove();
-              this.bullets.remove(b);
-            }  
-          }
-          this.bulletCollision();
-          this.shipCollision();
-          /*
-           *  update everything else
-           */
-
-           setTimeout( function() { 
-              if(this.SpaceRocks.Game.isRunning) { 
-                this.SpaceRocks.Game.update();
-              } 
-            } 
-            , 1000 / this.fps);
-         
       },
       
       bulletCollision: function(){
         var bulletLength = this.bullets.length;
         var asteroidsLength = this.asteroids.length;
-        for(var b = 0; b < bulletLength; b++){
-          for(var a = 0; a < asteroidsLength; a++){
-            console.log("bullets" + this.bullets);
-            console.log("asteroids" + this.asteroids);
+        for(var b = 0; b < bulletLength; b++) {
+          for(var a = 0; a < asteroidsLength; a++) {
             try{
-              if(distance(this.bullets[b].position, this.asteroids[a].position)<this.asteroidRadius)
-              {
+              if( distance(this.bullets[b].position, this.asteroids[a].position) < this.asteroidRadius) {
                 this.sounds.asteroidExplode.play();
                 this.bullets[b].obj.remove();
                 this.bullets.remove(b);
@@ -94,11 +62,13 @@
         }
       },
 
-      shipCollision: function(){
-        for (var a = 0; a <this.asteroids.length; a++){
-          if (distance(this.asteroids[a].position, this.ship.position)<this.asteroidRadius){
+      shipCollision: function() {
+        for (var a = 0; a < this.asteroids.length; a++){
+          if (distance(this.asteroids[a].position, this.ship.position) < this.asteroidRadius){
             console.log("Ship collision detected.");
             this.sounds.shipExplode.play();
+            this.ship.obj.remove();
+            this.stop();
           }
         }
       },
@@ -115,12 +85,27 @@
       },
       
       stop: function() {
-        console.log('Stop called!');
         this.isRunning = false;
+        this.restart();
       },
 
       restart: function() {
         console.log('Restart called!'); 
+        
+        if (this.ship && this.ship.obj) {
+          this.ship.obj.remove();
+        }
+        
+        for (var i=0; i < this.bullets.length; i++) {
+          this.bullets[i].obj.remove();
+        }
+        this.bullets = [];
+        
+        for (var i=0; i < this.asteroids.length; i++) {
+          this.asteroids[i].obj.remove();
+        }
+        this.asteroids = [];
+        
         this.start();
       },
       
@@ -130,14 +115,18 @@
         // increases speed if running
         if(this.isRunning){
           //makes the ship start to move
-          if(this.ship.speed == 0)
+          if(this.ship.speed == 0) {
             this.ship.speed = 1;
+          }
           //if the ship is moving under it's maximum speed, it is accelerated, otherwise nothing is done
-          else if(this.ship.speed < this.ship.MAX_SPEED){
+          else if(this.ship.speed < this.ship.MAX_SPEED) {
             this.ship.speed *= this.ship.ACCELERATION
           }
-          if(this.sounds.engine.paused)
+          
+          if (this.sounds.engine.paused) {
             this.sounds.engine.play();
+          }
+            
         }
       },
     
@@ -147,28 +136,25 @@
       },
 
       down: function() {
-        if(this.isRunning){
+        if (this.isRunning) {
           // decelerates if the speed is above 1
-          if(this.ship.speed > 1){
-            this.ship.speed /= this.ship.ACCELERATION
+          if (this.ship.speed > 1) {
+            this.ship.speed /= this.ship.ACCELERATION;
           }
           // if it is moving at minimum speed, it stops the ship
-          else this.ship.speed=0;
+          else this.ship.speed = 0;
           console.log(this.ship.speed);
         }
 
       },
     
-      left: function() {
-        console.log('left');
-        
+      left: function() {        
         if (this.isRunning) {
           this.ship.anglechange -= this.ship.ANGLE_DELTA_INCREMENT;
         }
       },
     
       right: function() {
-        console.log('right');
         if (this.isRunning) {
           this.ship.anglechange += this.ship.ANGLE_DELTA_INCREMENT;
         }
@@ -189,6 +175,27 @@
          console.log("misc debugging");
       }
     };
+    
+    // This is the game loop
+    
+    var update = function() {
+      this.ship.update();
+      for (var i=0; i < this.asteroids.length; i++) {
+        this.asteroids[i].updatePosition();
+      }
+      for( var b=0; b < this.bullets.length; b++ ) {
+        if (this.bullets[b].updatePosition()) {
+          this.bullets[b].obj.remove();
+          this.bullets.remove(b);
+        }  
+      }
+      this.bulletCollision();
+      this.shipCollision();
+      
+      if (this.isRunning) this.update();
+    };
+    
+    game.update = _.throttle( _.bind(update, game), 1000 / FPS);
 
     return game;
   })();
